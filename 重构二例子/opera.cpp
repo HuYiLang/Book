833ebdd655684eaa08e136a24535781ed80693a5
huyilang <1055449412@qq.com>
2019-05-12 12:30:54 +0800

    refactoring

---
+++ 重构二例子/opera.cpp
@@ -0,0 +1,36 @@
+/**** (C) COPYRIGHT 2019 huyilang ****
+* File Name       :  opera.cpp
+* Author          :  huyilang
+* Version         :  V1.0
+* Date            :  27/04/2019
+* Description     :  重构第一个例子
+***********************************/
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+string invoice[][2] = { { "Hamlet", "tragedy" }, { "as-like", "comedy" } };
+string plays[][2] = { { "customer", "Bigco" }, { "playID", "55" } };
+
+int statement(string invoice[][2], string plays[][2])
+{
+    int totalAmount = 0;
+    int volumeCredits = 0;
+    string result = " ";
+
+
+    return 0;
+}
+
+int main ()
+{
+    statement(invoice, plays);
+    cout << "hello world" << endl;
+    return 0;
+}
+
+/* EOF */
